80a4a5e0c51386fd5c0e8b34ffe26749ef217f77
GrimmettDS <davidsgrimmett@gmail.com>
2021-02-28 17:55:17 -0500

    Mass Update.  Still Needs Work.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,39 @@
+
 // from data.js
 var tableData = data;
+var tbody = d3.select('tbody');
+var btn = d3.select('button');
+var form = d3.select('#form')
 
-// YOUR CODE HERE!
+populateTable(tableData);
+btn.on('click', handleClick);
+form.on('submit', handleClick);
+
+// =============================================
+function populateTable(data) {
+    tbody.html("");
+
+    data.forEach(dataRow => {
+        var row = tbody.append('tr');
+
+        Object.values(dataRow).forEach(val => {
+            var cell = row.append('td');
+
+            cell.text(val);
+        });
+    });
+};
+
+function handleClick() {
+    d3.event.preventDefault();
+
+    var date = d3.select('input').property('value');
+    var filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    d3.select('input').property('value', '');
+    populateTable(filteredData);
+}
---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,53 @@
+// from data.js (given)
+var tableData = data;
+
+// Create variables from html
+var tbody = d3.select('tbody');
+var button = d3.select('button');
+var form = d3.select('#form');
+var input = d3.selectAll('input');
+
+populateTable(tableData);
+
+// Create event listener for "click" on the button and "return" submission
+button.on('click', handleClick);
+form.on('submit', handleClick);
+input.on('change', handleChange);
+
+// Create and display the table in index.html
+function populateTable(data) {
+    tbody.html("");
+
+    data.forEach(dataRow => {
+        var row = tbody.append('tr');
+
+        Object.values(dataRow).forEach(val => {
+            var cell = row.append('td');
+
+            cell.text(val);
+        });
+    });
+};
+
+var filteredData = tableData;
+
+//
+function handleChange() {
+
+    var key = d3.select(this).property('id');
+    var value = d3.select(this).property('value');
+
+    if (value) {
+        filteredData = filteredData.filter(row => row[key] === value);
+    };
+
+    populateTable(filteredData);
+}
+
+// Create and display the filtered table with only the submitted datetime value
+function handleClick() {
+    input.property("value", "");
+    filteredData = tableData;
+
+    populateTable(filteredData);
+}
